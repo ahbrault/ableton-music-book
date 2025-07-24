1e80e408e59da14e775b5b6898f33edc64a9e4f7
Aurélien Brault <ahbrault@gmail.com>
2025-07-24 16:22:02 +0000

    change section to article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       <main className="flex-1 relative mx-auto flex w-full max-w-8xl flex-auto justify-center sm:px-2 lg:px-8 xl:px-12">
         <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           {allChapters.map((chapter) => (
-            <section
+            <article
               key={chapter.slug}
               id={chapter.slug}
               className="chapter-section scroll-mt- mb-16 pb-16 border-b border-gray-200 last:border-b-0"
@@ -24,7 +24,7 @@ export default function HomePage() {
                 className="prose prose-lg max-w-none"
                 dangerouslySetInnerHTML={{ __html: chapter.html }}
               />
-            </section>
+            </article>
           ))}
         </div>
       </main>
